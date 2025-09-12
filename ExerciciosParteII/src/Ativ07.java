4b3c94beb6010bd19e81e98f0e92d29b18d3fa8c
Qiua <kowakurt@gmail.com>
2025-09-12 20:39:41 +0000

    Atualizando arquivos do projeto

--- ExerciciosParteII/src/Ativ07.java
+++ ExerciciosParteII/src/Ativ07.java
@@ -1,5 +1,11 @@
 public class Ativ07 {
     public static void main(String[] args) {
-        
+        int c = 12, d = 24;
+
+        if (c == d) {
+            System.out.println("São iguais");
+        } else {
+                System.out.println("Não são iguais");
+            }
+        }
     }
-}
