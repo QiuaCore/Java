0e8b89da7b1c01fe8177c3c755f8a0e18a374773
Qiua <kowakurt@gmail.com>
2025-09-15 14:28:46 -0300

    Update Ativ22.java
    Adicionado comentários

--- ExerciciosParteII/src/Ativ22.java
+++ ExerciciosParteII/src/Ativ22.java
@@ -1,12 +1,12 @@
 public class Ativ22 {
     public static void main(String[] args) {
-        int n = 9; boolean primo = true; //
+        int n = 9; boolean primo = true; // boleano neste caso, define primo como true, para verifição posterior se o mesmo se mantém primo
 
         if (n <= 1) {
             primo = false;
         } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
+            for (int i = 2; i <= Math.sqrt(n); i++) { // método Math.sqrt ira retornar a raiz quadrada positiva da variável
+                if (n % i == 0) { // ele pega a sobra da raiz quadrada e verifica se ainda é primo
                     primo = false;
                     break;
                 }
