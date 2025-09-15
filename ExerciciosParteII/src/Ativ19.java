3e79ca4ee07d5d388893c1553a3bb1a1686fe032
Qiua <kowakurt@gmail.com>
2025-09-15 10:50:02 -0300

    Update Ativ19.java

---
+++ ExerciciosParteII/src/Ativ19.java
@@ -0,0 +1,11 @@
+public class Ativ19 {
+    public static void main(String[] args) {
+      int i = 1; int soma = 0; // define primeiro as variaveis
+      while (i <= 100) { // While controla a informação?
+          soma = soma + i;
+          i++;
+
+          System.out.println("Soma total: " + soma);
+      }
+    }
+}
