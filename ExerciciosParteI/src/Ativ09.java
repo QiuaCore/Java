54c183ab6ef4cb215ced7a01fdf8f5905fe01c27
Qiua <kowakurt@gmail.com>
2025-09-15 11:01:28 -0300

    Update Ativ09.java

--- ExerciciosParteI/src/Ativ09.java
+++ ExerciciosParteI/src/Ativ09.java
@@ -2,6 +2,7 @@ public class Ativ09 {
     public static void main(String[] args) {
         int largura = 4;
         int altura = 6;
-        System.out.println("Área = " + 4*6);
+        System.out.println("Área = " + largura * altura);
     }
 }
+
