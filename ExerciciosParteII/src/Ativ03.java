fd252fef80153e2ae442a3ba13a98651fbc7ebb3
Qiua <kowakurt@gmail.com>
2025-09-15 10:58:50 -0300

    Update Ativ03.java

--- ExerciciosParteII/src/Ativ03.java
+++ ExerciciosParteII/src/Ativ03.java
@@ -2,7 +2,8 @@ public class Ativ03 {
     public static void main(String[] args) {
         int x = 5;
         x += 2;
-        int y = x;
+
         System.out.println("Valor Final: " + x);
     }
+
 }
