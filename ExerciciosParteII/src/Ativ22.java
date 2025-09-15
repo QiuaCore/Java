ed3dbc96135d344205c378527b31021fd813d9d1
Qiua <kowakurt@gmail.com>
2025-09-15 04:42:34 +0000

    Correção do código, erros de sintaxe corrigidos.

--- ExerciciosParteII/src/Ativ22.java
+++ ExerciciosParteII/src/Ativ22.java
@@ -1,11 +1,13 @@
 public class Ativ22 {
     public static void main(String[] args) {
-        int n = 9; boolean primo = false; //
+        int n = 9; boolean primo = true; //
 
         if (n <= 1) {
+            primo = false;
         } else {
             for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) {
+                    primo = false;
                     break;
                 }
             }
